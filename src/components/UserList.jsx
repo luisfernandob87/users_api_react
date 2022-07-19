744254716044df310a42de5f295b17d6c71e7587
luis.morales <luisfernandob87@gmail.com>
2022-07-19 14:02:01 -0600

    changes svg

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,26 +1,62 @@
-import React from 'react';
+import React from "react";
 
 const UserList = ({ users, selectUser, deleteUser }) => {
-
-
-    return (
-        <ul>
-            <h2>Users</h2>
-            {users.map((user)=> (
-                <li key={user.id}>
-                    <div className='data'>
-                        <h3>Name: {user.first_name}  {user.last_name}</h3>
-                        <p>Email: {user.email}</p>
-                        <p>Birthday: {user.birthday}</p>
-                    </div>
-                    <div className='button'>
-                        <button onClick={()=> deleteUser(user.id)}><img src="/src/img/delete.svg" alt="" /></button>
-                        <button onClick={()=> selectUser(user)}><img src="/src/img/edit.svg" alt="" /></button>
-                    </div>
-                </li> 
-            ))}     
-        </ul>
-    );
+  return (
+    <ul>
+      <h2>Users</h2>
+      {users.map((user) => (
+        <li key={user.id}>
+          <div className="data">
+            <h3>
+              Name: {user.first_name} {user.last_name}
+            </h3>
+            <p>Email: {user.email}</p>
+            <p>Birthday: {user.birthday}</p>
+          </div>
+          <div className="button">
+            <button onClick={() => deleteUser(user.id)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#ff2825"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <line x1="4" y1="7" x2="20" y2="7" />
+                <line x1="10" y1="11" x2="10" y2="17" />
+                <line x1="14" y1="11" x2="14" y2="17" />
+                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+              </svg>
+            </button>
+            <button onClick={() => selectUser(user)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="44"
+                height="44"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#000000"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                <line x1="16" y1="5" x2="19" y2="8" />
+              </svg>
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default UserList;
